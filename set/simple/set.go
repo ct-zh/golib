56ebc13ca7a3ca6b038477b4ff45d2d5739dc869
caoting <307298900@qq.com>
2021-03-30 01:11:58 +0800

    feature:set

---
+++ set/simple/set.go
@@ -0,0 +1,18 @@
+package simple
+
+// 最简单的set实现
+
+type Set map[string]struct{}
+
+func (s Set) Has(key string) bool {
+	_, ok := s[key]
+	return ok
+}
+
+func (s Set) Add(key string) {
+	s[key] = struct{}{}
+}
+
+func (s Set) Delete(key string) {
+	delete(s, key)
+}
---
+++ set/simple/set_test.go
@@ -0,0 +1,26 @@
+package simple
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSet_Add(t *testing.T) {
+	Convey("add", t, func() {
+		set := make(Set)
+		set.Add("aaaa")
+		So(set.Has("aaaa"), ShouldBeTrue)
+		So(set.Has("bbbb"), ShouldBeFalse)
+	})
+}
+
+func TestSet_Delete(t *testing.T) {
+	Convey("delete", t, func() {
+		set := make(Set)
+		set.Add("aaaa")
+		So(set.Has("aaaa"), ShouldBeTrue)
+		set.Delete("aaaa")
+		So(set.Has("aaaa"), ShouldBeFalse)
+	})
+}
