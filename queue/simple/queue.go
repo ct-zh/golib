a682c9969d151f9824e5681f401a2c1e7ae9cf8a
caoting <307298900@qq.com>
2021-04-13 20:15:26 +0800

    sync queue

--- queue/simple/queue.go
+++ queue/simple/queue.go
@@ -1,6 +1,8 @@
 package simple
 
-type queue interface {
+import "sync"
+
+type Queue interface {
 	LPush(v interface{})
 	LPop() interface{}
 	RPush(v interface{})
@@ -11,7 +13,7 @@ type queue interface {
 
 type simpleQueue []interface{}
 
-func New() *simpleQueue {
+func New() Queue {
 	return &simpleQueue{}
 }
 
@@ -42,4 +44,50 @@ func (q *simpleQueue) IsEmpty() bool {
 
 func (q *simpleQueue) Len() int {
 	return len(*q)
+}
+
+// 附带锁的queue
+type syncQueue struct {
+	mu sync.Mutex
+	q  []interface{}
+}
+
+func NewSyncQueue() Queue {
+	return &syncQueue{}
+}
+
+func (s *syncQueue) LPush(v interface{}) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.q = append([]interface{}{v}, s.q...)
+}
+
+func (s *syncQueue) LPop() interface{} {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	head := s.q[0]
+	s.q = s.q[1:]
+	return head
+}
+
+func (s *syncQueue) RPush(v interface{}) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.q = append(s.q, v)
+}
+
+func (s *syncQueue) RPop() interface{} {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	tail := s.q[len(s.q)-1]
+	s.q = s.q[0 : len(s.q)-1]
+	return tail
+}
+
+func (s *syncQueue) IsEmpty() bool {
+	return len(s.q) == 0
+}
+
+func (s *syncQueue) Len() int {
+	return len(s.q)
 }
--- queue/simple/queue_test.go
+++ queue/simple/queue_test.go
@@ -7,37 +7,47 @@ import (
 	"testing"
 )
 
+func tQueue(q Queue) {
+	So(q.IsEmpty(), ShouldBeTrue)
+	data := []int{1, 2, 3, 4, 5}
+	for _, datum := range data {
+		q.RPush(datum)
+	}
+	So(q.IsEmpty(), ShouldBeFalse)
+
+	for _, datum := range data {
+		So(q.LPop().(int), ShouldEqual, datum)
+	}
+
+	So(q.IsEmpty(), ShouldBeTrue)
+	for _, datum := range data {
+		q.LPush(datum)
+	}
+	So(q.IsEmpty(), ShouldBeFalse)
+
+	for _, datum := range data {
+		So(q.RPop().(int), ShouldEqual, datum)
+	}
+	So(q.IsEmpty(), ShouldBeTrue)
+}
+
 func TestQueue(t *testing.T) {
-	Convey("simple/simpleQueue", t, func() {
-		q := New()
-
-		So(q.IsEmpty(), ShouldBeTrue)
-		data := []int{1, 2, 3, 4, 5}
-		for _, datum := range data {
-			q.RPush(datum)
-		}
-		So(q.IsEmpty(), ShouldBeFalse)
-
-		for _, datum := range data {
-			So(q.LPop().(int), ShouldEqual, datum)
-		}
-
-		So(q.IsEmpty(), ShouldBeTrue)
-		for _, datum := range data {
-			q.LPush(datum)
-		}
-		So(q.IsEmpty(), ShouldBeFalse)
-
-		for _, datum := range data {
-			So(q.RPop().(int), ShouldEqual, datum)
-		}
-		So(q.IsEmpty(), ShouldBeTrue)
+	Convey("queue", t, func() {
+		Convey("simple/simpleQueue", func() {
+			q := New()
+			tQueue(q)
+		})
+		Convey("syncQueue", func() {
+			q2 := NewSyncQueue()
+			tQueue(q2)
+		})
 	})
 }
 
 // 并发测试; list是线程不安全的
 func TestParallel(t *testing.T) {
-	q := New()
+	//q := New()
+	q := NewSyncQueue()
 
 	// 总共20 * 500 = 10000 次push
 	wg := sync.WaitGroup{}
