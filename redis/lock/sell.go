f4821e8d69a95887ee71f20c55c651f2414b2a27
caoting <307298900@qq.com>
2021-04-14 22:21:13 +0800

    update: redis lock

---
+++ redis/lock/sell.go
@@ -0,0 +1,44 @@
+package lock
+
+import (
+	"errors"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/LannisterAlwaysPaysHisDebts/goLearn/src/redis/redigo"
+	"github.com/gomodule/redigo/redis"
+)
+
+// 分布式锁的简易实现, 解决超卖问题
+
+const fooKey = "test:foo1"
+
+const max = 1000
+
+// 使用redigo附带的分布式锁
+func getOne(conn redis.Conn) (bool, error) {
+	lock := redigo.GetLock("test:lock", time.Duration(20)*time.Second)
+	lock.Lock()
+	defer lock.Unlock()
+	reply, err := conn.Do("GET", fooKey)
+	if err != nil {
+		return false, err
+	}
+
+	val := 0
+	if v, ok := reply.([]byte); ok {
+		val, _ = strconv.Atoi(string(v))
+		if val >= max {
+			return false, errors.New("out of range")
+		}
+	}
+
+	reply2, err := conn.Do("INCR", fooKey)
+	if err != nil {
+		return false, err
+	}
+	log.Printf("get val: %d, reply val: %d", val, reply2)
+
+	return true, nil
+}
---
+++ redis/lock/sell_test.go
@@ -0,0 +1,94 @@
+package lock
+
+import (
+	"github.com/LannisterAlwaysPaysHisDebts/goLearn/src/redis/redigo"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"sync"
+	"testing"
+)
+
+//
+func TestGetOne(t *testing.T) {
+	pool := getPool()
+	conn := pool.Get().(redis.Conn)
+	conn.Do("DEL", fooKey)
+
+	const WorkerNum = 10
+
+	dataCh := make(chan int, 10)
+	wg := sync.WaitGroup{}
+	wg.Add(WorkerNum)
+
+	// 生产者
+	go func() {
+		for i := 0; i < 2000; i++ {
+			dataCh <- i
+		}
+		close(dataCh)
+	}()
+
+	// 消费者
+	for i := 0; i < WorkerNum; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case _, open := <-dataCh:
+					if !open {
+						return
+					}
+					conn := pool.Get().(redis.Conn)
+					ok, err := getOne(conn)
+					if err != nil {
+						t.Fatal("getOne error: ", err)
+					}
+					if !ok {
+						t.Fatal("get one failed")
+					}
+					pool.Put(conn)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func getPool() *sync.Pool {
+	return &sync.Pool{New: func() interface{} {
+		redigo.Init("127.0.0.1", "6379", "")
+		conn, err := redigo.GetConn()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return conn
+	}}
+}
+
+func doFunc(pool *sync.Pool,
+	b *testing.B,
+	fn func(redis.Conn) (bool, error)) {
+
+	conn := pool.Get().(redis.Conn)
+	ok, err := fn(conn)
+	if err != nil {
+		b.Fatal("getOne error: ", err)
+	}
+	if !ok {
+		b.Fatal("get one failed")
+	}
+	pool.Put(conn)
+}
+
+func BenchmarkGetOne(b *testing.B) {
+	pool := getPool()
+	conn := pool.Get().(redis.Conn)
+	conn.Do("DEL", fooKey)
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doFunc(pool, b, getOne)
+		}
+	})
+}
