7aa3267f807a3afb25b0d490b7dcc0808f428e1e
caoting <307298900@qq.com>
2023-03-30 11:03:34 +0800

    updaate

--- usage/threading/group2.go
+++ usage/threading/group2.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+
+	"github.com/ct-zh/golib/usage/tls"
 )
 
 // A Group is a collection of goroutines working on subtasks that are part of the same overall task.
@@ -55,5 +57,59 @@ func (g *Group) do(ctx context.Context, f func(ctx2 context.Context) error) {
 			})
 		}
 	}()
+	tls.For(ctx, func() {
+		err = f(ctx)
+	})
+}
 
+// GOMAXPROCS set max goroutine to work.
+// GOMAXPROCS设置最大goroutine为工作。
+func (g *Group) GOMAXPROCS(n int) *Group {
+	if n <= 0 {
+		n = 1
+	}
+	g.workerOnce.Do(func() {
+		g.ch = make(chan call, n)
+		for i := 0; i < n; i++ {
+			go func() {
+				for c := range g.ch {
+					g.do(c.ctx, c.f)
+				}
+			}()
+		}
+	})
+	return g
+}
+
+// Go calls the given function in a new goroutine within tls.For.
+// Go在tls.For中的新goroutine中调用给定的函数。
+// Go will recover if any panic occurs.
+// Go将恢复任何紧急情况。
+// The _ctx will passed to f.
+// The First error will be returned by Wait.
+func (g *Group) Go(ctx context.Context, f func(ctx context.Context) error) {
+	g.wg.Add(1)
+	if g.ch != nil {
+		select {
+		case g.ch <- call{ctx, f}:
+		default:
+
+		}
+	}
+	go g.do(ctx, f)
+}
+
+// Wait blocks until all function calls from the Go method have returned, then returns the first non-nil error (if any) from them.
+// 直到Go方法的所有函数调用都返回，然后从中返回第一个非零错误（如果有）
+func (g *Group) Wait() error {
+	if g.ch != nil {
+		for _, f := range g.chs {
+			g.ch <- f
+		}
+	}
+	g.wg.Wait()
+	if g.ch != nil {
+		close(g.ch) // let all receiver exit
+	}
+	return g.err
 }
