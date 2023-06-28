230804da4391bef717f27312436b0115784b5e60
caoting <307298900@qq.com>
2023-06-28 14:46:46 +0800

    move

---
+++ usage/config/base_config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+// 通用config配置文件读取方法
+
+type Config struct {
+} // 通用配置
+
+func NewConfig() *Config {
+	cfg := &Config{}
+	cfgFile, err := ioutil.ReadFile("./config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(cfgFile, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
